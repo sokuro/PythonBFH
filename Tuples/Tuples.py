f7f522b4bba2fe5fff9e34823316b40c2841e942
Kajo <ugorcak@gmx.ch>
2018-03-17 18:52:00 +0100

    Tuples print handling extended

--- Tuples/Tuples.py
+++ Tuples/Tuples.py
@@ -2,6 +2,7 @@
 * Tuples: IMMUTABLE ordered sequence of items
 * Similar to Lists, but elements are enclosed within parentheses & their size can NOT be changed (like lists)
 * Expression separated by commas
+* Brackets optional
 '''
 tuple1 = ('string 1', 12, 15.0, 'karol', 22)
 tuple2 = 'string'
