fda372a5464394fde0e65397a33bb292c14e382a
Kajo <ugorcak@gmx.ch>
2018-03-18 19:06:20 +0100

    Variable Caching added

---
+++ Basis/Caching/Caching.py
@@ -0,0 +1,14 @@
+"""
+* Memoization: Caching computed values in order to avoid repeated costly computations
+"""
+
+
+def costly_computation(x):
+    return 42
+
+
+def cached_compute(x, _cache={}):
+    if x not in _cache:
+        _cache[x] = costly_computation(x)
+
+    return _cache[x]
