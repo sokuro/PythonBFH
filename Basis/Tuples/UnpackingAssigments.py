e9fddb45bac651e5601c20f27100a68e745cd6e2
Kajo <ugorcak@gmx.ch>
2018-03-18 12:05:40 +0100

    Tuples handling of unpacked assignments

---
+++ Basis/Tuples/UnpackingAssigments.py
@@ -0,0 +1,10 @@
+"""
+* An iterable can be unpacked into individual items
+* Formatted f-string literal
+"""
+
+temp = ('Karol', 1981, 'SK')
+
+name, year, nationality = temp      # unpacking assigment
+
+print(f'{name} was born in {year} and is from {nationality}.')
