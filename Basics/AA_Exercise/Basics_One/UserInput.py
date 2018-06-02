4ee9836844bbf377a24acaf8b346f24ae52dbb5a
Kajo <ugorcak@gmx.ch>
2018-06-02 17:19:13 +0200

    User String Input Handling

---
+++ Basics/AA_Exercise/Basics_One/UserInput.py
@@ -0,0 +1,10 @@
+"""
+    Accept User Input
+    Print it swapped
+"""
+
+
+firstName = input('Please, enter your first Name: ')
+lastName = input('Please, enter your last Name: ')
+
+print('Hello ' + firstName + ' ' + lastName + ' !')
