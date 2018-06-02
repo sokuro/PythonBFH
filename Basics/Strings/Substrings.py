58eb7da41818e16197d91e7a19b54f4f6713a28f
Kajo <ugorcak@gmx.ch>
2018-06-02 17:48:42 +0200

    splitting String

--- Basics/Strings/Substrings.py
+++ Basics/Strings/Substrings.py
@@ -6,3 +6,12 @@ print(str[1])
 print(str[3:8])
 print(str[3:])
 print((str + '\n') * 3)     # concatenation
+
+
+""" 
+    split String 
+    sep= separate by Char
+    maxsplit= List with maxsplit+1 Elements
+"""
+stringSplit = str.rsplit(sep='m', maxsplit=3)
+print(stringSplit)
