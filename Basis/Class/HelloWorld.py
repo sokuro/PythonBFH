3546dae9c2bf150e63b872d87edfdb6e8baca70f
Kajo <ugorcak@gmx.ch>
2018-06-02 10:15:35 +0200

    simple Class Structure Repetition

---
+++ Basis/Class/HelloWorld.py
@@ -0,0 +1,36 @@
+"""
+    classic Class
+"""
+
+
+class MyClass:
+    """ instantiation Operation """
+
+    def __init__(self, str):
+        self._str = str
+
+    """ attribute Reference """
+    i = 123
+
+    """ Method Example """
+
+    def f(self):
+        print(f'{self._str}')
+
+
+""" Main Method """
+
+
+def main():
+    """
+        create a new Instance of MyClass
+    """
+
+    """ assign the Object to a local Variable """
+    greeting = MyClass('hello world')
+
+    greeting.f()
+
+
+""" call the Main Method """
+main()
