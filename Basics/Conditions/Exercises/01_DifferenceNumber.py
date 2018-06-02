1d2c0b206b05da666b1fbfd801fc21887bf668aa
Kajo <ugorcak@gmx.ch>
2018-06-02 20:13:01 +0200

    Conditions Numbers Differencies

---
+++ Basics/Conditions/Exercises/01_DifferenceNumber.py
@@ -0,0 +1,14 @@
+"""
+    get the difference between a given number and 17, if the number is greater than 17 return double the absolute difference.
+"""
+
+
+def difference(n):
+    if n <= 17:
+        return 17 - n
+    else:
+        return (n - 17) * 2
+
+
+print(difference(17))
+print(difference(22))
