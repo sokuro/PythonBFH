1c32587fb75fcf77aa266863427a24ed034902b5
Kajo <ugorcak@gmx.ch>
2018-06-03 16:21:26 +0200

    is Char a Vowel

---
+++ Basics/Strings/Exercises/07_IsVowel.py
@@ -0,0 +1,15 @@
+"""
+    test whether a passed letter is a vowel or not
+"""
+
+
+def is_vowel(char):
+
+    all_vowels = 'aeiou'
+
+    return char in all_vowels
+
+
+print(is_vowel('a'))
+print(is_vowel('b'))
+print(is_vowel('tu'))
