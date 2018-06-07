0520a5f3b0f656c2be82c317ad2f6f01ab9ae958
Kajo <ugorcak@gmx.ch>
2018-06-07 18:39:27 +0200

    global Variable explicitly set as global

--- 02_LanguageElements/Namespaces/LocalNamespace.py
+++ 02_LanguageElements/Namespaces/LocalNamespace.py
@@ -14,3 +14,20 @@ def local_var():
 
 local_var()             # >> 20
 print(my_var)           # >> 10
+
+print('\n')
+
+"""
+    by defining the Variable global inside the function
+"""
+
+
+def local_var_2():
+    global my_var
+    print(my_var)       # global Variable: >> 10
+    my_var = 20
+    print(my_var)
+
+
+local_var_2()           # >> 20
+print(my_var)           # >> 20
