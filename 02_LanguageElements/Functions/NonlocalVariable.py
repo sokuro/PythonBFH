5ba5f065ff227d880372a21073f361e6ace2def4
Kajo <ugorcak@gmx.ch>
2018-06-08 11:18:32 +0200

    added Commentary

--- 02_LanguageElements/Functions/NonlocalVariable.py
+++ 02_LanguageElements/Functions/NonlocalVariable.py
@@ -3,21 +3,21 @@
 """
 
 
-def outer_function(x):
-    y = x + 1
+def outer_function(x):      # x = 7
+    y = x + 1               # y = 8
 
     # nested function
-    def inner_function(z):
+    def inner_function(z):  # z = y = 8
         nonlocal y          # bind Variable from the outer Scope
-        y += 1
-        return x + y + z
+        y += 1              # y + 1 = 8 + 1 = 9
+        return x + y + z    # 7 + 9 + 8
 
     return inner_function(y)
 
 
 def main():
     r = outer_function(7)
-    print(r)
+    print(r)                # >> 24
 
 
 main()
