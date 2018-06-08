e4970bb0836e4baabf298f990be11660abade287
Kajo <ugorcak@gmx.ch>
2018-06-08 20:25:42 +0200

    Filter even List Items

---
+++ 01_Fundamentals/Filter/01_Filter.py
@@ -0,0 +1,32 @@
+"""
+    Filter returns an Iterator that produces the Items Xi from the Iterator X for which the predicate p(Xi) is true
+"""
+
+
+""" keep only even Elements in a List """
+l = list(range(5))
+
+
+# With Loop
+def filter_evens():
+
+    list_even = []
+
+    for num in l:
+        if num % 2 == 0:
+            list_even.append(num)
+
+    return list_even
+
+
+# filter with Lambda
+def filter_lambda():
+    return list(filter(lambda x: x % 2 == 0, l))
+
+
+def main():
+    print(filter_evens())
+    print(filter_lambda())
+
+
+main()
