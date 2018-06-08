0c1db8f724351bc171f651e720faca263810bf89
Kajo <ugorcak@gmx.ch>
2018-06-08 16:44:17 +0200

    Non-negative Descriptor for the Class Order corrected

--- Exercises_BFH/Exam/05_Person.py
+++ Exercises_BFH/Exam/05_Person.py
@@ -1,4 +1,28 @@
 """
+    Class: PositiveAttribute Descriptor
+    Negative Attributes of the Class 'Person' are not allowed!
+"""
+
+
+class PositiveAttribute:
+
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        if value < 0:
+            # raise ValueError('Cannot be negative!')
+            if self.name == 'age':
+                raise ValueError('Age can not be negative!')
+            elif self.name == 'salary':
+                raise ValueError('Salary can not be negative!')
+        instance.__dict__[self.name] = value
+
+
+"""
     Class: Person
     Attributes: age, salary
 """
@@ -6,25 +30,32 @@
 
 class Person:
 
+    # implementing the Descriptor
+    age = PositiveAttribute('age')
+    salary = PositiveAttribute('salary')
+
     def __init__(self, name, age, salary):
-        self._name = name
-        self._age = age
-        self._salary = salary
+
+        self.name = name
+        self.age = age
+        self.salary = salary
 
     def __repr__(self):
-        return 'Person({}, {}, {})'.format(self._name, self._age, self._salary)
+        return 'Person({}, {}, {})'.format(self.name, self.age, self.salary)
 
 
-"""
-    Class: PositiveAttribute
-    Negative Attributes of the Class 'Person' are not allowed!
-"""
+def main():
+    import doctest
+    # doctest.testmod()
+
+    bob = Person('Alice Brown', 42, 123000)
+    print(bob.__repr__())
+
+    charlie = Person('Charlie Davis', -1, 5000)
+    print(charlie.__repr__())
+
+    charlie = Person('Fox Mulder', 57, -5000)
+    print(charlie.__repr__())
 
 
-class PositiveAttribute:
-
-    def __init__(self, age, salary):
-        self._age = age
-        self._salary = salary
-
-    
+main()
