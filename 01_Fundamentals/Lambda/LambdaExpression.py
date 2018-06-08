5403c2fa5bf248d235fe1465516bf16d1d73607b
Kajo <ugorcak@gmx.ch>
2018-06-08 18:53:19 +0200

    using timeit to measure Function call Time

--- 01_Fundamentals/Lambda/LambdaExpression.py
+++ 01_Fundamentals/Lambda/LambdaExpression.py
@@ -16,8 +16,18 @@ f = lambda x, y, z: x + y + z
 
 
 def main():
-    print(func(1, 2, 3))
-    print(f(1, 2, 3))
+
+    """
+        measure call time of the Function with 'timeit'
+    """
+    import timeit
+    # print(func(1, 2, 3))
+    print(timeit.timeit('func(1, 2, 3)', setup='from __main__ import func'))
+
+    print('\n')
+
+    # print(f(1, 2, 3))
+    print(timeit.timeit('f(1, 2, 3)', setup='from __main__ import f'))
 
 
 main()
