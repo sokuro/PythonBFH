92f3d0b67a8ea61dd2a47b74e591d90026e18fea
Kajo <ugorcak@gmx.ch>
2018-06-10 17:15:53 +0200

    Decorator's inner Function extended by a Statement using Decorator Annotation

---
+++ 01_Fundamentals/Decorators/03_Decorator_Annotation.py
@@ -0,0 +1,40 @@
+"""
+    Decorator: Function that takes another Function as Argument
+
+    Extend the Wrapper with an extra Statement
+
+    Use Annotation
+"""
+
+
+# Decorator
+def decorator_function(original_function):
+
+    def wrapper_function():
+
+        # extra Statement
+        print('wrapper executed this before {}'.format(original_function.__name__))
+
+        # pass the Argument directly into the inner Function
+        # execute the Function
+        original_function()
+
+    # by removing the () the Function is waiting to be executed
+    return wrapper_function
+
+
+# Annotaton for the Decorator Function
+@decorator_function
+def display():
+
+    print('display Function')
+
+
+def main():
+
+    # display Function passed into the Decorator Function
+    # Annotation used!
+    display()
+
+
+main()
