23aaa1fc5fa8b3fcf504713fb3d0bdd1e3baab64
Kajo <ugorcak@gmx.ch>
2018-06-10 16:51:21 +0200

    Decorators with Arguments added

---
+++ 01_Fundamentals/Decorators/02_DecoratorsWithArgument.py
@@ -0,0 +1,33 @@
+"""
+    Decorators with Arguments
+
+    Closure
+"""
+
+
+# extend with Variable
+def outer_function(msg):
+
+    # assign to the passed Variable
+    message = msg
+
+    def inner_function():
+
+        print(message)
+
+    # by removing the () the Function is waiting to be executed
+    return inner_function
+
+
+def main():
+
+    # assign to a Variable
+    hi_func = outer_function("Hi")
+    bye_func = outer_function("Bye")
+
+    # using Closure
+    hi_func()       # >> Hi
+    bye_func()      # >> Bye
+
+
+main()
