b6a2e23b4f28135a4ab67ec552dab355eaa39808
Kajo <ugorcak@gmx.ch>
2018-06-10 17:07:31 +0200

    Decorator added

---
+++ 01_Fundamentals/Decorators/Decorator.py
@@ -0,0 +1,33 @@
+"""
+    Decorator: Function that takes another Function as Argument
+"""
+
+
+# Decorator
+def decorator_function(original_function):
+
+    def wrapper_function():
+
+        # pass the Argument directly into the inner Function
+        # execute the Function
+        original_function()
+
+    # by removing the () the Function is waiting to be executed
+    return wrapper_function
+
+
+# Display the Decorator
+def display():
+
+    print('display Function')
+
+
+def main():
+
+    # display Function passed in
+    decorated_display = decorator_function(display)
+
+    decorated_display()
+
+
+main()
