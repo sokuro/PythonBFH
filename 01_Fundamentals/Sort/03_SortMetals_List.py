878c45a70e7ad7b03e970deac288a08aff2f6a4b
Kajo <ugorcak@gmx.ch>
2018-06-10 14:40:44 +0200

    Sort a List using 'sorted'

---
+++ 01_Fundamentals/Sort/03_SortMetals_List.py
@@ -0,0 +1,14 @@
+"""
+    sorted(iterable, key, reversed)
+"""
+
+
+earth_metals = ["Beryllium", "Magnesium", "Calcium", "Strontium", "Barium", "Radium"]
+
+
+# the original List stays UNALTERED!!!
+sorted_earth_metals = sorted(earth_metals)
+
+print(sorted_earth_metals)      # new created List
+
+print(earth_metals)             # original List
