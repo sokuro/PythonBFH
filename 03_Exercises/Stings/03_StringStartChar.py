02d8a29db5784d0299a7d7bd96b12c84f8b174ec
ugorcak <ugorcak@gmx.ch>
2018-06-11 13:12:59 +0200

    Start of the entered String

---
+++ 03_Exercises/Stings/03_StringStartChar.py
@@ -0,0 +1,9 @@
+"""
+    check whether a string starts with specified characters
+"""
+
+
+str_input = input('Enter a String: ')
+char_input = input('Enter a Character: ')
+
+print(str_input[0:1] == char_input)
